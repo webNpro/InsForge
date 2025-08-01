911821d39daad35c3fa2ac758db48e61d577dd2a
jwfing <jwfing@gmail.com>
2025-08-01 15:40:56 -0700

    update logflare version

--- docker-init/db/logs.sql
+++ docker-init/db/logs.sql
@@ -6,13 +6,4 @@ CREATE DATABASE _insforge WITH OWNER :pguser;
 create schema if not exists _analytics;
 alter schema _analytics owner to :pguser;
 
--- Create publication for Logflare replication (if it doesn't exist)
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_publication WHERE pubname = 'logflare_pub') THEN
-        CREATE PUBLICATION logflare_pub FOR ALL TABLES;
-    END IF;
-END
-$$;
-
-\c postgres
+\c insforge
