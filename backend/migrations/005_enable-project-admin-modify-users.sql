4dd43232a02e045cd18712c75999730fc09086a3
jwfing <jwfing@gmail.com>
2025-09-08 20:24:52 -0700

    clean up migrate sql

--- backend/migrations/005_enable-project-admin-modify-users.sql
+++ backend/migrations/005_enable-project-admin-modify-users.sql
@@ -1,27 +1,7 @@
--- Migration: 004 - Enable project admin modify any users.
+-- Migration: 005 - Enable project admin modify any users.
 
 DO $$
 BEGIN
-    -- Create a function to reload PostgREST schema
-    CREATE OR REPLACE FUNCTION reload_postgrest_schema()
-    RETURNS void
-    LANGUAGE plpgsql
-    AS $reload_function$
-    BEGIN
-        -- Method 1: Use NOTIFY to signal PostgREST to reload schema
-        -- PostgREST listens to 'pgrst' channel for schema changes
-        NOTIFY pgrst, 'reload schema';
-        
-        RAISE NOTICE 'PostgREST schema reload notification sent';
-    END
-    $reload_function$;
-
-    -- Grant execute permission to project_admin and authenticated users
-    GRANT EXECUTE ON FUNCTION reload_postgrest_schema() TO project_admin;
-    GRANT EXECUTE ON FUNCTION reload_postgrest_schema() TO authenticated;
-    
-    RAISE NOTICE 'PostgREST schema reload function created successfully';
-
     IF NOT EXISTS (
       SELECT 1 FROM pg_policies 
       WHERE tablename = 'users' 
