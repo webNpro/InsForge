0b7d6a76bf85e339a2d672b6ecbaff291a76c8cc
jwfing <jwfing@gmail.com>
2025-09-08 20:49:49 -0700

    chore: format

--- backend/migrations/005_enable-project-admin-modify-users.sql
+++ backend/migrations/005_enable-project-admin-modify-users.sql
@@ -2,22 +2,24 @@
 
 DO $$
 BEGIN
+    -- Create policy to allow project_admin to update any user
     IF NOT EXISTS (
-      SELECT 1 FROM pg_policies 
-      WHERE tablename = 'users' 
-      AND policyname = 'Allow project_admin to update any user'
+        SELECT 1 FROM pg_policies 
+        WHERE tablename = 'users' 
+        AND policyname = 'Allow project_admin to update any user'
     ) THEN
-      CREATE POLICY "Allow project_admin to update any user" ON users
-        FOR UPDATE
-        TO project_admin
-        USING (true)
-        WITH CHECK (true);  -- make sure project-admin always can update
+        CREATE POLICY "Allow project_admin to update any user" ON users
+            FOR UPDATE
+            TO project_admin
+            USING (true)
+            WITH CHECK (true);  -- Ensure project_admin always can update
     END IF;
 
+    -- Grant necessary permissions
     GRANT SELECT, UPDATE ON users TO project_admin;
     
     -- Notify PostgREST to reload schema after policy changes
-     IF EXISTS (SELECT 1 FROM pg_proc WHERE proname = 'reload_postgrest_schema') THEN
+    IF EXISTS (SELECT 1 FROM pg_proc WHERE proname = 'reload_postgrest_schema') THEN
         PERFORM reload_postgrest_schema();
         RAISE NOTICE 'PostgREST schema reload requested after migration';
     ELSE
