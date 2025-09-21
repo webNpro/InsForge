71da43a3dba4fbcad97cb413ad0f21d8f4a8273a
Lyu <fermioniclyu@gmail.com>
2025-09-21 15:55:23 -0600

    fix users tab UI

--- frontend/src/features/auth/types.ts
+++
@@ -1,8 +0,0 @@
-import type { UserSchema } from '@insforge/shared-schemas';
-
-export interface User extends UserSchema {
-  provider_type: string;
-  identities: {
-    provider: string;
-  }[];
-}
