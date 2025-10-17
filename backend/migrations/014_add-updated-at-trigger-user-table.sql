04396534b0692290125f4ee427c1f63b1bc45ce5
Abinand P <abinand0911@gmail.com>
2025-10-17 13:55:39 +0530

    refactor: following the migration to use the same function and trigger from migration 000
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- backend/migrations/014_add-updated-at-trigger-user-table.sql
+++ backend/migrations/014_add-updated-at-trigger-user-table.sql
@@ -1,20 +1,8 @@
 -- Migration: 014 - Add updated_at trigger to users table
 -- Adds the updated_at trigger to the users table for automatic timestamp management
 
-DO $$
-BEGIN
-    -- Add updated_at trigger to users table if not exists
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_trigger 
-        WHERE tgrelid = 'users'::regclass 
-        AND tgname = 'users_update_timestamp'
-    ) THEN
-        CREATE TRIGGER users_update_timestamp 
-        BEFORE UPDATE ON users
-        FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-        
-        RAISE NOTICE 'Added updated_at trigger to users table';
-    ELSE
-        RAISE NOTICE 'updated_at trigger already exists on users table';
-    END IF;
-END $$;
+
+DROP TRIGGER IF EXISTS update_users_updated_at ON users;
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
