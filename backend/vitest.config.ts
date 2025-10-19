35cadd25fa54750524f01e2bb87474e8284f78f1
Yasir761 <myasiransari16@gmail.com>
2025-10-19 17:29:31 +0530

    add _dirname in vite.config.ts file to make it error proof

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
 
     resolve: {
